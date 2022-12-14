8a85fc2b417846a11e78a4f40d66957512d9169a
Beat Hagenlocher <bah@posteo.de>
2022-12-14 18:15:54 +0100

    Add working - but distorted - sphere.

--- ray.c
+++ ray.c
@@ -6,7 +6,7 @@
 /*   By: bhagenlo <bhagenlo@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 15:25:40 by bhagenlo          #+#    #+#             */
-/*   Updated: 2022/12/14 17:15:33 by bhagenlo         ###   ########.fr       */
+/*   Updated: 2022/12/14 18:09:52 by bhagenlo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,18 +32,28 @@ void	del_ray(t_ray *r)
 	// ft_free(r->dir);
 }
 
+bool	hit_sphere(t_3d center, double radius, t_ray r)
+{
+	t_3d	oc;
+
+    oc = *sum_3d(r.pos,  *mul(-1, center));
+    double a = dot(r.dir, r.dir);
+    double b = 2.0 * dot(oc, r.dir);
+    double c = dot(oc, oc) - radius*radius;
+    double discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
 int	color_ray(t_ray r)
 {
 	t_3d	*unit;
 	double	t;
 	uint32_t clr;
 
-	// print3d("orig", r.pos);
-	// print3d("dir ", r.dir);
+	if (hit_sphere((t_3d){0, 0, -1}, 0.5, r))
+		return (dcolor(1.0, 0, 0));
 	unit = mk_unit(r.dir);
-	// print3d("unit", *unit);
 	t = 0.5 * (unit->y + 1.0);
-	// printf("t: %f\n", t);
 	t_clr	s = (t_clr){255, 255, 255};
 	t_clr	e = (t_clr){127, 180, 255};
 	clr = color(s, e, t);
