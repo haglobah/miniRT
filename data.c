f739fd06f9af89dc3d8340d0c15f984e81d5fcdc
Beat Hagenlocher <bah@posteo.de>
2023-01-11 14:19:32 +0100

    Add data.c

---
+++ data.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   data.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhagenlo <bhagenlo@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/11 14:12:49 by bhagenlo          #+#    #+#             */
+/*   Updated: 2023/01/11 14:18:46 by bhagenlo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+t_ambient	*mk_amb(t_d ratio, t_3d *clr)
+{
+	
+}
+t_cam		*mk_cam(t_3d *pos, t_3d *dir, t_d fov)
+{
+	
+}
+t_light		*mk_l(t_3d *pos, t_3d *clr, t_d brightness)
+{
+	
+}
+t_sphere	*mk_sp(t_3d *pos, t_d diameter, t_3d *clr)
+{
+	
+}
+t_plane		*mk_pl(t_3d *pos, t_3d *normal, t_3d *clr)
+{
+	
+}
+t_cyl		*mk_cyl(t_3d *pos, t_3d *normal, t_d diameter, t_d height, t_3d *clr)
+{
+	
+}
